6e74c457e20293255e038ea9e59c71e511836c90
yashpriyam <yashpriyam74@gmail.com>
2019-08-11 02:52:19 +0530

    changing urls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   storage: :s3,
+  Paperclip::Attachment.default_options[:url] = ':s3_domain_url',
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename',
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
